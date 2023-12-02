049b9ceaf9855467d20ef7334114f42a6c4d875b
JunedSetiawan <junedhayko502@gmail.com>
2023-12-02 09:16:06 +0700

    update: contact component

--- src/components/starter/footer/footer.tsx
+++ src/components/starter/footer/footer.tsx
@@ -7,7 +7,7 @@ export default component$(() => {
   return (
     <footer>
       <div class="mt-8 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl lg:mt-16">
-        <div class="container text-xl md:text-3xl font-semibold text-center rounded font-mono">
+        <div class="container text-lg md:text-3xl font-semibold text-center rounded font-mono">
           `Created by <span class="text-primary-focus">Lazy Programmer`</span>
         </div>
       </div>
