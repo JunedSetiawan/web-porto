fad9384e57e94b129a24e7002010041d2ad93df9
Jnd <70498944+JunedSetiawan@users.noreply.github.com>
2023-12-23 11:28:02 +0700

    Update about.tsx

--- src/components/starter/about/about.tsx
+++ src/components/starter/about/about.tsx
@@ -9,12 +9,11 @@ export default component$(() => {
           <hr class="border-primary border-2 w-36 mt-3" />
           <div class="ml-3">
             <p class="font-medium">
-              Fresh Graduate with Intermediate Skilled Laravel Developer with a
-              background in web application development and a passion for
-              creating efficient and innovative solutions. Experienced in
-              designing databases. Ability to work collaboratively in a team and
-              deliver high quality code. Committed to continuously learning and
-              staying up-to-date with the latest technology.
+              Hi i'm Juned Setiawan, Student at Politeknik Elektronika Negeri Surabaya with majoring in informatics engineering.
+
+I'm a Fresh Graduate with Skilled Laravel Developer with a background in web application development and a passion for creating efficient and innovative solutions. Experienced in designing databases. Ability to work collaboratively in a team and committed to continuously learning and staying up-to-date with the latest technology.
+
+In the dynamic realm of web development, I stand as a passionate Laravel developer, adept at transforming ideas into tangible digital experiences. My journey began with a fascination for computers, which blossomed into a deep-seated passion for unraveling the intricacies of web development. And i've thrived in diverse teams, fostering a spirit of innovation and shared knowledge.
             </p>
           </div>
         </div>
